98e6c8b48c6876d1704d69276831acd5cce1ae25
Juan <juancarive@gmail.com>
2023-10-29 12:39:39 -0500

    Resumiendo texto

--- crear_contenido.py
+++ crear_contenido.py
@@ -29,8 +29,8 @@ def resumir_text(texto, tokens, temperatura, modelo="text-davici-002"):
     )
     return respuesta.choices[0].text.strip()
 
-tema = input("Elije un tema para tu artículo")
-tokens = int(input("Cuántos tokens máximos tendrá tu artículo: "))
-temperatura = int(input("Del 1 al 10, qué tan creativo quuieres que sea tu artículo?: "))/10
-articulo_creado = crear_contenido(tema, tokens, temperatura)
-print(articulo_creado)
+original = input("Pega aquí el artículo que quieres resumir")
+tokens = int(input("Cuántos tokens máximos tendrá tu resumen: "))
+temperatura = int(input("Del 1 al 10, qué tan creativo quieres que sea tu artículo?: "))/10
+resumen = resumir_texto(original, tokens, temperatura)
+print(resumen)
