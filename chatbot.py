1f430dd30ace944344b59fa95d97d46cbd812a95
Juan <juancarive@gmail.com>
2023-10-28 22:36:04 -0500

    Se generan variables para almacenar conversaciones y dar contexto

--- chatbot.py
+++ chatbot.py
@@ -6,6 +6,8 @@ load_dotenv()
 api_key = os.getenv("OPENAI_APY_KEY")
 openai.api_key = api_key
 
+preguntas_anteriores = []
+respuestas_anteriores = []
 
 def preguntar_chat_gpt(prompt, modelo="text-davinci-002"):
     respuesta = openai.Completion.create(
@@ -21,11 +23,19 @@ def preguntar_chat_gpt(prompt, modelo="text-davinci-002"):
 print("Bienvenido a nuestro chatbot básico. Escribe 'salir' cuando quieras términar")
 
 while True:
+    conversacion_historica = ""
     ingreso_usuario = input("\nTú: ")
     if ingreso_usuario.lower() == "salir":
         break
 
-    prompt = f"El usuario pregunta: {ingreso_usuario}\nChatGPT responde:"
-    respuesta_gpt = preguntar_chat_gpt(prompt)
-    print(f"Chatbot: {respuesta_gpt}")
+    for pregunta, respuesta in zip(preguntas_anteriores, respuestas_anteriores):
+        conversacion_historica += f"El usuario pregunta: {pregunta}\n"
+        conversacion_historica += f"ChatGPT responde: {respuesta}\n" 
 
+    prompt = f"El usuario pregunta: {ingreso_usuario}\n"
+    conversacion_historica += prompt
+    respuesta_gpt = preguntar_chat_gpt(conversacion_historica)
+    print(f"{respuesta_gpt}")
+    
+    preguntas_anteriores.append(ingreso_usuario)
+    respuestas_anteriores.append(respuesta_gpt)
